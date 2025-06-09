900172d2cbd767cdd506c6e5a08e2424b175b601
GenozidAdan <20223tn159@utez.edu.mx>
2025-06-09 17:44:54 -0600

    Calculadora funcional

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+let valor1 = 2;
+let valor2 = 4;
+
+let suma = valor1 + valor2;
+console.log("El valor de la suma es "+suma)
+
+let resta = valor1 - valor2;
+console.log("El valor de la resta es "+resta)
+
+let multiplicacion = valor1 * valor2;
+console.log("El valor de la multiplicación es "+multiplicacion)
+
+let division = valor1 / valor2;
+console.log("El valor de la division es "+division)
